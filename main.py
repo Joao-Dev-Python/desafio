5cdf482b105e904ba4a40e1c43581679399e1c2d
joao <joaoadmob2@gmail.com>
2022-05-05 12:05:15 -0300

    webhook

--- main.py
+++ main.py
@@ -1,45 +1,20 @@
+from crypt import methods
 from flask import Flask,jsonify,request
-from flask_restful import reqparse
-from flask_cors import CORS
-import  os, base64
 
-message = "Em terra de quem tem um olho e rei"
-print(message)
-message_bytes = message.encode('ascii')
-base64_bytes = base64.b64encode(message_bytes)
-base64_message = base64_bytes.decode('ascii')
-print(base64_message)
-
-
-
-path_params = reqparse.RequestParser()
-path_params.add_argument('r', type=str)
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/pro",methods = ['GET'])
-def pro():
-    return jsonify({
-        "instrucao" : """Preste muita atencao 'vc tera que descriptografar a mensagem da chave DESAFIO com base64 corrigi-la e manda de volta no endpoint  ?r=  encriptografada em base64' na rota \resp. Antes de encriptografar removatodos todos os espacos e nao use caracter especial caso contrario retornara um erro """,
-        
-        "DESAFIO": base64_message})
-    
-    
-    
+events = []
 
-@app.route('/resp', methods=["GET"])
-def resp():
-    data =   path_params.parse_args()
-    if(data["r"] == "RW10ZXJyYWRlY2Vnb3F1ZW10ZW11bW9saG9lcmVp"):
-        return jsonify({
-            "message" : "muito bem aguarde o proximo desafio",
-            "resp" : data["r"]
-            }), 201
-    return jsonify({
-        "message" : "Vc nao fez correto"
-    })
-    
 
+@app.route("/ev",methods= ['POST'])
+def post_events():
+    data = request.get_json()
+    events.append(data)
+    return jsonify(data)
+
+@app.route("events",methods=['GET'])
+def get_events():
+    return jsonify(events)
 
 
 if __name__== '__main__':
